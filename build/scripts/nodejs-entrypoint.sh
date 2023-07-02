ef1aa6d6124e1b45114698a622b427ef5b8059a3
Joel Ferreira <joejr93@gmail.com>
2023-07-02 12:21:52 -0300

    update

--- build/scripts/nodejs-entrypoint.sh
+++ build/scripts/nodejs-entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Aguarde o MySQL estar pronto para conexões
-until mysqladmin ping -hdb -uroot -proot -s; do
-    echo "Aguardando o MySQL estar pronto..."
-    sleep 1
-done
-
 # Defina as permissões corretas para a pasta do MySQL
 chmod -R 777 /var/lib/mysql
 
